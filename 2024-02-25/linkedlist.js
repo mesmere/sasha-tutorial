fc73d04a1141c2ac926ac978b2b196b38e03df67
mesmere <95945959+mesmere@users.noreply.github.com>
2024-02-06 05:28:47 -0500

    Added 2024-02-25 content

---
+++ 2024-02-25/linkedlist.js
@@ -0,0 +1,118 @@
+class Node {
+  data;
+  next = undefined;
+
+  constructor(data) {
+    this.data = data;
+  }
+}
+
+class LinkedList {
+  head = undefined;
+
+  constructor(items = []) {
+    for (const item of items.reverse()) {
+      this.insertAtHead(item);
+    }
+  }
+
+  get(k) {
+    if (this.head === undefined) {
+      throw "Out of range.";
+    }
+
+    let cur = this.head;
+    while (--k >= 0) {
+      if (cur.next === undefined) {
+        throw "Out of range.";
+      }
+      cur = cur.next;
+    }
+    return cur.data;
+  }
+
+  getRecursive(k) {
+    function helper(node, k) {
+      if (node === undefined) {
+        throw "Out of range.";
+      } 
+      if (k === 0) {
+        return node.data;
+      }
+      return helper(node.next, k-1);
+    }
+
+    return helper(this.head, k);
+  }
+
+  insertAtHead(data) {
+    const oldHead = this.head;
+    this.head = new Node(data);
+    this.head.next = oldHead;
+  }
+
+  insert(k, data) {
+    // Special case to update head
+    if (k === 0) {
+      this.insertAtHead(data);
+      return;
+    }
+
+    if (this.head === undefined) {
+      throw "Out of range.";
+    }
+
+    let cur = this.head;
+    while (--k >= 1) { // Stop short!
+      if (cur.next === undefined) {
+        throw "Out of range.";
+      }
+      cur = cur.next;
+    }
+
+    const oldNext = cur.next;
+    cur.next = new Node(data);
+    cur.next.next = oldNext;
+  }
+
+  removeAtHead() {
+    if (this.head === undefined) {
+      throw "Out of range.";
+    }
+    this.head = this.head.next;
+  }
+
+  remove(k) {
+    // Special case to update head
+    if (k === 0) {
+      this.removeAtHead();
+      return;
+    }
+
+    if (this.head === undefined) {
+      throw "Out of range.";
+    }
+
+    let cur = this.head;
+    while (--k >= 1) { // Stop short!
+      if (cur.next === undefined) {
+        throw "Out of range.";
+      }
+      cur = cur.next;
+    }
+
+    cur.next = cur.next.next;
+  }
+}
+
+const list = new LinkedList(["zero", "one"]);
+list.insert(2, "two");
+list.insert(3, "three");
+list.removeAtHead();
+list.remove(2);
+console.log(`Head is ${list.get(0)}.`);
+try {
+  list.get(2);
+} catch (e) {
+  console.log(`Tried to get(2), caught: ${e}`);
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 * 2024-01-20 [code](/2024-01-20/)
 * 2024-01-27 [code](/2024-01-27/) [slides](https://docs.google.com/presentation/d/1ZU1S-ZhoI_-bXazIfg6JkOwQXDx_djzdLavJxFDFiK8/view) ([pdf](/2024-01-27/sasha-2024-01-27.pdf))
 * 2024-02-03 [code](/2024-02-03/) [slides](https://docs.google.com/presentation/d/1thINXRRLwl9bR6Xm4dQDM6bMMBishLQkRVXrVdLk3rc/view) ([pdf](/2024-02-03/sasha-2024-02-03.pdf))
+* 2024-02-25 [code](/2024-02-25/) [slides](https://docs.google.com/presentation/d/156DYQYw2F-hF58pjns9EaldSmho9-wCD0cHHbJDKVsc/view) ([pdf](/2024-02-25/sasha-2024-02-25.pdf))
